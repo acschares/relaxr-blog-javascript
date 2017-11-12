5c5d6353bfbd0573aaf21ac1227b26aa119e01ea
acschares <32584155+acschares@users.noreply.github.com>
2017-11-11 21:47:03 -0800

    Finished homework

--- js/index.js
+++ js/index.js
@@ -7,4 +7,21 @@ $(document).ready(function(){
         $(".readmore").hide();
     });
   })
+
+  $('.readless').click(function(event){
+    event.preventDefault();
+
+    $("#show-this-on-click").slideUp("slow", function(){
+        $(".readless").hide();
+        $(".readmore").show();
+    });
+  })
+
+  $('.learnmore').click(function(event){
+    event.preventDefault();
+
+    $("#learnmoretext").slideDown("slow", function(){
+        $(".learnmore").hide();
+    });
+  })
 });
